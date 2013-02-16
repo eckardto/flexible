0154ec2b0d2857abd71a8c9223fbd352b3ede427
Eckardto <olekardt@gmail.com>
2013-02-16 15:26:40 -0500

    Update examples

--- examples/simple.js
+++ examples/simple.js
@@ -7,11 +7,11 @@ var flexible = require('../index.js');
 // Initiate and a crawler. Chainable.
 flexible.crawl({uri: 'http://www.google.com', interval: 140})
     .use(flexible.querystring)
-    .use(flexible.router);
+    .use(flexible.router)
 
     .route('*', function (req, res, body, queue_item) {
         console.log('Everything is handled by this route.');
     })
 
-    .on('complete', function () {console.log('Finished!');});
+    .on('complete', function () {console.log('Finished!');})
     .on('error', function (error) {console.error(error);});
