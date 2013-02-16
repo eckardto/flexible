a52f0ae246b522ca1acdabadfa6870762ab0c103
Eckardto <olekardt@gmail.com>
2013-02-16 15:23:08 -0500

    Update examples

--- examples/complex.js
+++ examples/complex.js
@@ -3,18 +3,19 @@
 'use strict';
 
 var flexible = require('../index.js');
+var request = require('request');
 
 var crawler = new flexible.Crawler();
 
 // Use SQLite based queue.
 crawler.queue = new flexible.SqliteQueue();
 
-
 crawler.max_concurrency = 5;
 crawler.interval = 100;
 crawler.max_redirects = 4;
 crawler.headers = {'User-Agent': 'Node/Flexible (https://github.com/Eckardto/flexible)'};
 crawler.domains = ['www.google.com'];
+crawler.jar = request.jar();
 
 // Setup middleware.
 crawler.use(flexible.querystring);
